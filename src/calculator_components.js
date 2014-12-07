ff045dd3c21fb86161d261e484562f158c06ed73
andy koch <koch.andy@gmail.com>
2014-12-07 00:48:19 -0800

    re-organizing code into files

--- src/calculator_components.js
+++ src/calculator_components.js
@@ -6,51 +6,6 @@
   Now stand back, and let me work code maijck.
 */
 
-
-var Calculator = React.createClass({
-  getInitialState: function() {
-    return {
-      homePrice: 400000,
-      depositAmount: 80000,
-      amortizationPeriod: 25,
-      interestRate: 3.5
-    };
-  },
-
-  loanAmount: function() {
-    var loanAmount = this.state.homePrice - this.state.depositAmount;
-    return (loanAmount > 0) ? loanAmount : 0;
-  },
-
-  onChange: function(state, newValue) {
-    var newState = {};
-    newState[state] = Number(newValue);
-    this.setState(newState);
-  },
-
-  render: function() {
-    var loanAmount = this.loanAmount();
-    console.log(this.state);
-    return (
-      <form role="form">
-        <HomePrice
-          onChange={this.onChange}
-          homePrice={this.state.homePrice} />
-        <DepositAmount
-          onChange={this.onChange}
-          depositAmount={this.state.depositAmount} />
-        <LoanAmount loanAmount={loanAmount} />
-        <InterestRate
-          onChange={this.onChange}
-          interestRate={this.state.interestRate} />
-        <AmortizationPeriod
-          onChange={this.onChange}
-          amortizationPeriod={this.state.amortizationPeriod} />
-      </form>
-    );
-  }
-});
-
 var HomePrice = React.createClass({
   updateHomePrice: function(e) {
     this.props.onChange('homePrice', e.target.value);
@@ -67,7 +22,6 @@ var HomePrice = React.createClass({
 
 var DepositAmount = React.createClass({
   updateDepositAmount: function(e) {
-    console.log(e.target.value);
     this.props.onChange('depositAmount', e.target.value);
   },
 
@@ -126,7 +80,3 @@ var AmortizationPeriod = React.createClass({
     );
   }
 });
-
-React.render(<Calculator />,
-  document.getElementById('calculator')
-);
